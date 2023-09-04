dc1db78c4bf2a2c86ec1ea0c3543e21e88bef069
rk <kumar.rohit512000@gmail.com>
2023-09-04 18:12:40 +0530

    a

---
+++ walkAppSinvo/Model/collectionViewModel2.swift
@@ -0,0 +1,19 @@
+//
+//  collectionViewModel2.swift
+//  walkAppSinvo
+//
+//  Created by Rajeev on 04/09/23.
+//
+
+import Foundation
+struct collect2
+{
+    let imageName:String
+    let imageNamee:String
+    init(_Imgname:String, _imagenamee:String)
+    {
+        
+        self.imageName = _Imgname
+        self.imageNamee = _imagenamee
+    }
+}
